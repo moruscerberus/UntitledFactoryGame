4dbb8de309b1754a4caafbcc0654a75325f39787
Jörgen Gullstrand <moruscerberus@gmail.com>
2024-06-17 17:45:57 +0200

    Added files via upload

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,52 @@
+package engineTester;
+
+import models.RawModel;
+import models.TexturedModel;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.OBJLoader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+import entities.Camera;
+import entities.Entity;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
+
+		RawModel model = OBJLoader.loadObjModel("agera", loader);
+
+		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("no_texture")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,-20,-50),0,0,0,5);
+		
+		Camera camera = new Camera();
+		
+		while(!Display.isCloseRequested()){
+			entity.increaseRotation(0, 0, 0);
+			camera.move();
+			renderer.prepare();
+			shader.start();
+			shader.loadViewMatrix(camera);
+			renderer.render(entity,shader);
+			shader.stop();
+			DisplayManager.updateDisplay();
+		}
+
+		shader.cleanUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+
+	}
+
+}
