e114f783d79d98f6c5f9bd9d81fcc267ea448b58
Jörgen Gullstrand <moruscerberus@gmail.com>
2024-06-19 18:47:17 +0200

    Implemented batch renderering

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -7,14 +7,17 @@ import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
+import renderEngine.*;
 import shaders.StaticShader;
 import textures.ModelTexture;
 import entities.Camera;
 import entities.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.lwjgl.Sys.getTime;
 
 public class MainGameLoop {
 
@@ -23,33 +26,39 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
-		Renderer renderer = new Renderer(shader);
+		MasterRenderer renderer = new MasterRenderer();
+		List<Entity> allCubes = new ArrayList<Entity>();
 
-		RawModel model = OBJLoader.loadObjModel("dragon", loader);
+		Random random = new Random();
+		RawModel model = OBJLoader.loadObjModel("cube", loader);
+		TexturedModel cubeModel = new TexturedModel(model, new ModelTexture(
+				loader.loadTexture("no_texture")));
 
-		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("no_texture")));
-		ModelTexture texture = staticModel.getTexture();
-		texture.setShineDamper(10);
-		texture.setReflectivity(1);
-		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
-		Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
-		
-		Camera camera = new Camera();
-		
-		while(!Display.isCloseRequested()){
-			entity.increaseRotation(0, 1, 0);
-			camera.move();
-			renderer.prepare();
-			shader.start();
-			shader.loadLight(light);
-			shader.loadViewMatrix(camera);
-			renderer.render(entity,shader);
-			shader.stop();
-			DisplayManager.updateDisplay();
+
+		Light light = new Light(new Vector3f(3000, 2000, 3000), new Vector3f(1, 1, 1));
+
+		for (int i = 0; i < 200; i++) {
+			float x = random.nextFloat() * 100 - 50;
+			float y = random.nextFloat() * 100 - 50;
+			float z = random.nextFloat() * -300;
+			allCubes.add(new Entity(cubeModel, new Vector3f(x, y, z), random.nextFloat() * 180f, random.nextFloat() * 180f, 0f, 1f));
 		}
 
-		shader.cleanUp();
+		Camera camera = new Camera();
+
+
+
+		while(!Display.isCloseRequested()){
+			camera.move();
+
+			for (Entity cube : allCubes) {
+				renderer.processEntity(cube);
+			}
+
+			renderer.render(light, camera);
+			DisplayManager.updateDisplay();
+		}
+		renderer.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -16,6 +16,9 @@ import toolbox.Maths;
 
 import entities.Entity;
 
+import java.util.List;
+import java.util.Map;
+
 public class Renderer {
 	
 	private static final float FOV = 70;
@@ -23,8 +26,12 @@ public class Renderer {
 	private static final float FAR_PLANE = 1000;
 	
 	private Matrix4f projectionMatrix;
+	private StaticShader shader;
 	
-	public Renderer(StaticShader shader){
+	public Renderer(StaticShader shader) {
+		this.shader = shader;
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glEnable(GL11.GL_BACK);
 		createProjectionMatrix();
 		shader.start();
 		shader.loadProjectionMatrix(projectionMatrix);
@@ -37,29 +44,46 @@ public class Renderer {
 		GL11.glClearColor(0.45f, 0.45f, 0.45f, 1);
 	}
 
-	public void render(Entity entity, StaticShader shader) {
-		TexturedModel model = entity.getModel();
+	public void render(Map<TexturedModel, List<Entity>> entities) {
+		for(TexturedModel model : entities.keySet()) {
+			prepareTexturedModel(model);
+			List<Entity> batch = entities.get(model);
+			for(Entity entity : batch) {
+				prepareInstance(entity);
+				GL11.glDrawElements(GL11.GL_TRIANGLES, model.getRawModel().getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+			}
+			unbindTexturedModel();
+		}
+	}
+
+	private void prepareTexturedModel(TexturedModel model) {
 		RawModel rawModel = model.getRawModel();
 		GL30.glBindVertexArray(rawModel.getVaoID());
 		GL20.glEnableVertexAttribArray(0);
 		GL20.glEnableVertexAttribArray(1);
 		GL20.glEnableVertexAttribArray(2);
-		Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(),
-				entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
-		shader.loadTransformationMatrix(transformationMatrix);
 		ModelTexture texture = model.getTexture();
 		shader.loadShineVariables(texture.getShineDamper(), texture.getReflectivity());
 		GL13.glActiveTexture(GL13.GL_TEXTURE0);
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getID());
-		GL11.glDrawElements(GL11.GL_TRIANGLES, rawModel.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
-		GL20.glDisableVertexAttribArray(0);
-		GL20.glDisableVertexAttribArray(1);
-		GL20.glDisableVertexAttribArray(2);
 		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
 		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
 
+	}
+
+	private void unbindTexturedModel() {
+		GL20.glDisableVertexAttribArray(0);
+		GL20.glDisableVertexAttribArray(1);
+		GL20.glDisableVertexAttribArray(2);
 		GL30.glBindVertexArray(0);
 	}
+
+	private void prepareInstance(Entity entity) {
+		Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(),
+				entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
+		shader.loadTransformationMatrix(transformationMatrix);
+	}
+
 	
 	private void createProjectionMatrix(){
 		float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
